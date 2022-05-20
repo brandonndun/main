ffcce0eabd8ac13eb45922ebea31e4c085d94051
vanggelina10 <75229252+vanggelina10@users.noreply.github.com>
2022-05-20 14:55:17 +0700

    Add item

---
+++ resources/views/product.blade.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.12.9/dist/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+    <title>Product  - Durian Asun</title>
+</head>
+<body>
+    <div class="container">
+        <div class="card mt-5" style="width: 24rem; border-radius: 10px;">
+            <div class="d-flex justify-content-around">
+                <img class="card-img-top" src="image-gallery.png" alt="Card image cap" style="width: 300px;">
+            </div>
+            <div class="card-body" style="background-color: #57B846;">
+                <h5 class="card-title">Nama Produk</h5>
+                <div class="row d-flex justify-content-center">
+                    <div class="col-6 d-flex justify-content-center">
+                        <a href="#" class="btn ml-5" style="background-color: #D0F2A4; color:black;">Edit</a>
+                    </div>
+                    <div class="col-6 d-flex justify-content-center">
+                        <a href="#" class="btn mr-5" style="background-color: #D0F2A4; color:black;">Delete</a>
+                    </div>
+                </div>
+                <div class="row d-flex justify-content-around mt-3">
+                    <div class="col-6 col-md-4 d-flex justify-content-center">
+                        <a href="#" class="btn" style="background-color: #D0F2A4; color:black;">Add</a>
+                    </div>
+                    <div class="col-6 col-md-4 d-flex justify-content-center">
+                        <a href="#" class="btn" style="background-color: #D0F2A4; color:black;">Reduce</a>
+                    </div>
+                    <div class="col-6 col-md-4 d-flex justify-content-center">
+                        <a href="#" class="btn" style="background-color: #D0F2A4; color:black;">Send</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
