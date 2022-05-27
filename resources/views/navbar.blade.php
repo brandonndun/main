55c219895bcea98c2cfcd2e7d7d72e31aa085f86
vanya vimala <vanyavimala2015@gmail.com>
2022-05-27 09:44:43 +0700

    logout button

--- resources/views/navbar.blade.php
+++ resources/views/navbar.blade.php
@@ -6,7 +6,9 @@
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Quicksand:wght@300;400;500;600;700&display=swap" rel="stylesheet">
     <link rel="" href="send.css"> 
-    <title>Navbar</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+ <script src="https://kit.fontawesome.com/f43daa6159.js" crossorigin="anonymous"></script>
+    <title>Durian Asun</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
 </svg>
 </head>
@@ -14,7 +16,7 @@
     <nav class="navbar navbar-expand-lg navbar-light" style="background-color : #D0F2A4">
       <div class="container-fluid">
         <a class="navbar-brand" href="home" style="z-index: 2">
-          <img src="img/Asun-Logo-Header-250.webp" style="width : 100px; margin-left:40px;" alt="">
+          <img src="img/Asun-Logo-Header-250.webp" style="width : 100px; margin-left:70px;" alt="">
         </a>
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
           <span class="navbar-toggler-icon"></span>
@@ -33,6 +35,7 @@
             <a class="nav-link active" style="margin : 0 12px" aria-current="page" href="/transaksi">Transaksi</a>
             </li>
           </ul>
+          <a style="color:black;position:absolute;right:80px;" href="/"><i class="fa-solid fa-power-off"></i></a>
         </div>
       </div>
     </nav>
