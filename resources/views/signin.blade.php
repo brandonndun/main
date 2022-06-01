4f38c5e2e055218efa8facb14f70aef35b9a0e87
brandonndun <75228937+brandonndun@users.noreply.github.com>
2022-06-01 09:30:38 +0700

    .

--- resources/views/signin.blade.php
+++ resources/views/signin.blade.php
@@ -7,11 +7,11 @@
  <link rel="shortcut icon" href="img/Asun-Logo-Header-250 copy.webp" type="image/x-icon">
 </head>
 <body>
-    <div class="logodurian" style="text-align:center; margin-top:  35px;" >
-        <img src="img/Asun-Logo-Header-250.webp"  style="width: 15%;"  >
+    <div class="logodurian" style="text-align:center; margin-top:  35px;">
+        <img src="img/Asun-Logo-Header-250.webp"  style="width: 15%; margin: 11px; margin-left: -7px"  >
     </div>
     
-    <div class="wrapper" style="border-style: ridge; box-shadow: 10px 10px 5px grey; margin-top:20px" >
+    <div class="wrapper" style="border: outset; box-shadow: 10px 10px 5px grey; margin-top:20px" >
         <div class="wrapper-header">
             <!-- <div class="icon"><i class="fa fa-sign-in fa-2x"></i></div> -->
             <h2 class="title">Sign In</h2>
