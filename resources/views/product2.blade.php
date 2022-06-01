6a87d22b23e071dfd491d9a9eaf641dcfe0a0fc6
vanggelina10 <75229252+vanggelina10@users.noreply.github.com>
2022-06-01 09:43:59 +0700

    update shortcut iconproduct2

--- resources/views/product2.blade.php
+++ resources/views/product2.blade.php
@@ -21,7 +21,8 @@
 
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
-</head>
+        <link rel="shortcut icon" href="img/Asun-Logo-Header-250 copy.webp" type="image/x-icon">
+    </head>
 
 <body>
 
