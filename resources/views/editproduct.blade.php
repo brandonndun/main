b4965dd1b35729381afdc08b76aa7c21833d76d4
vanggelina10 <75229252+vanggelina10@users.noreply.github.com>
2022-06-10 15:28:19 +0700

    tes(stashed)

--- resources/views/editproduct.blade.php
+++ resources/views/editproduct.blade.php
@@ -10,7 +10,7 @@
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
     <title>Edit Product  - Durian Asun</title>
-    <link rel="shortcut icon" href="img/Asun-Logo-Header-250 copy.webp" type="image/x-icon">
+    <link rel="shortcut icon" href="img/Asun-Logo-Header-250.webp" type="image/x-icon">
 </head>
 <body>
 @include("navbar")
--- resources/views/sendproduct.blade.php
+++ resources/views/sendproduct.blade.php
@@ -22,7 +22,7 @@
 
     <div class="gambar" style="display:flex">
         <div class="image">
-            <img src="img/icon-image-512.webp" style="width : 300px; margin-left: 296px; margin-top: 37px;" class="img-fluid" alt="...">
+            <img src="img/icon-image-512.webp" style="width : 300px; margin-left: 308px; margin-top: 37px;" class="img-fluid" alt="...">
         </div>
         <div class="inputbox" style="display:flex; flex-direction: column;">
             <div class="form-nama-produk">
