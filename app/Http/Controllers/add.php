579769b80b6da2e1f4e6654fedc8cda89c9d18e3
vanggelina10 <75229252+vanggelina10@users.noreply.github.com>
2022-06-15 17:45:08 +0700

    send_product ke pencatatan
    SUDAH

--- app/Http/Controllers/add.php
+++ app/Http/Controllers/add.php
@@ -29,4 +29,17 @@ class add extends Controller
         $insert = $model->insert_produk($id_produk, $supplier, $harga, $warehouse, $jumlah, $status);
         return redirect('/product');
     }
+
+    Public function transaksi(Request $request){
+        $produk = $request->input('nama');
+        $id_produk = $request->input('idproduk');
+        $supplier = $request->input('supplier');
+        $warehouse = $request->input('Warehouse');
+        $harga = $request->input('Harga');
+        $jumlah = $request->input('jumlah');
+        $status = $request->input('Pembayaran');
+        $model = new signup;
+        $insert = $model->insert_produk($id_produk, $supplier, $harga, $warehouse, $jumlah, $status);
+        return redirect('/product');
+    }
 }
