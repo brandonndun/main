66b36305597599c8849719522b7501dd87134b91
Jason Widjaja <75228907+jason-widjaja@users.noreply.github.com>
2022-06-16 16:25:02 +0700

    a

--- config/app.php
+++ config/app.php
@@ -193,7 +193,6 @@ return [
 
     'aliases' => Facade::defaultAliases()->merge([
         // 'ExampleClass' => App\Example\ExampleClass::class, 
-        'Datatables' => Yajra\DataTables\Facades\Datatables::class,
     ])->toArray(),
 
 ];
